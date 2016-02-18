79ddc3d9ddd47d8635de7c8f57806d23e503210e
Alex Seeholzer <seeholzer@gmail.com>
2016-02-18 14:18:56 +0100

    travis plotting problems

--- neurodynex/test/test_LIF.py
+++ neurodynex/test/test_LIF.py
@@ -1,11 +1,14 @@
-from neurodynex.leaky_integrate_and_fire.LIF import LIF_Sinus, LIF_Step
+import matplotlib
+matplotlib.use('Agg')  # needed for plotting on travis
 
 
 def test_runnable_Step():
     """Test if LIF_Step is runnable."""
+    from neurodynex.leaky_integrate_and_fire.LIF import LIF_Step
     LIF_Step()
 
 
 def test_runnable_Sinus():
     """Test if LIF_Sinus is runnable."""
+    from neurodynex.leaky_integrate_and_fire.LIF import LIF_Sinus
     LIF_Sinus()
